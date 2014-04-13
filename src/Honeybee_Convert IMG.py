51071a797912199402d767b63743ba326a493cfb
Mostapha Sadeghipour Roudsari <sadeghipour@gmail.com>
2014-04-13 19:13:15 -0400

    Initial commit
    I added this new component based on a wish in this Grasshopper thread
    (http://www.grasshopper3d.com/group/ladybug/forum/topics/honeybee-falsecolor).
    The component let the user to convert different types of image files to
    each other.

---
+++ src/Honeybee_Convert IMG.py
@@ -0,0 +1,66 @@
+# By Mostapha Sadeghipour Roudsari
+# Sadeghipour@gmail.com
+# Honeybee started by Mostapha Sadeghipour Roudsari is licensed
+# under a Creative Commons Attribution-ShareAlike 3.0 Unported License.
+
+
+"""
+Convert Image
+-
+Provided by Honeybee 0.0.51
+    
+    Args:
+        _imageFilePath: Path to an image file(BMP, GIF, JPEG, PNG, TIFF)
+        _targetImageType_: 0>"BMP", 1>"GIF", 2>"Jpeg", 3>"PNG", 4>"TIFF"
+    Returns:
+        convertedFilePath: New file path
+"""
+
+ghenv.Component.Name = "Honeybee_Convert IMG"
+ghenv.Component.NickName = 'IMG>IMG'
+ghenv.Component.Message = 'VER 0.0.51\nAPR_13_2014'
+ghenv.Component.Category = "Honeybee"
+ghenv.Component.SubCategory = "7 | WIP"
+try: ghenv.Component.AdditionalHelpFromDocStrings = "1"
+except: pass
+
+import clr
+clr.AddReference("System.Drawing")
+
+from System.Drawing import Image
+from System.Drawing.Imaging import ImageFormat
+import os
+
+imageFromat = { 0 : ImageFormat.Bmp,
+1 : ImageFormat.Gif,
+2 : ImageFormat.Jpeg,
+3 : ImageFormat.Png,
+4 : ImageFormat.Tiff,
+}
+
+imageExt = { 0 : "BMP",
+1 : "GIF",
+2 : "Jpeg",
+3 : "PNG",
+4 : "TIFF",
+}
+
+def main(imageFilePath, targetImageType):
+    try:
+        filePath, ext = os.path.splitext(imageFilePath)
+        img = Image.FromFile(imageFilePath)
+        
+        targetEx = imageExt[targetImageType%5]
+        
+        # make sure target ext is not same as input!
+        if targetEx.lower()!= ext[1:].lower():
+            outputFilePath = ".".join([filePath, targetEx])
+            img.Save(outputFilePath, imageFromat[targetImageType%5])
+        
+            return outputFilePath
+            
+    except Exception, e:
+        return "something went wrong: " + str(e)
+        
+if _imageFilePath:
+    convertedFilePath = main(_imageFilePath, _targetImageType_)
