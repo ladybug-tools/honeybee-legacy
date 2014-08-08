fb80c73a8066c5a69496a3ce7e91b28f125a6f1d
Chris Mackey <chris@mackeyarchitecture.com>
2014-08-08 11:23:46 -0400

    Separated Surface Outputs in Result Writer

--- src/Honeybee_Write EP Result Parameters.py
+++ src/Honeybee_Write EP Result Parameters.py
@@ -15,7 +15,9 @@ Provided by Honeybee 0.0.53
         zoneGainsAndLosses_: Set to "True" to have EnergyPlus solve for building gains and losses such as people gains, solar gains and infiltration losses/gains.
         zoneComfortMetrics_: Set to "True" to have EnergyPlus solve for the mean air temperature, mean radiant temperature, operative temperature, and relative humidity of each zone.
         zoneHVACParams_: Set to "True" to have EnergyPlus solve for the fractions of heating/cooling loads that are latent vs. sensible as well as the the flow rate and temperature of supply air into each zone.
-        surfaceAnalysis_: Set to "True" to have EnergyPlus solve for an number of factors that relate to the individual surfaces bodering each zone, such as gains and losses through the surfaces as well as interior/exterior surface temperatures.
+        surfaceAnalysis_: Set to "True" to have EnergyPlus solve for the interior and exterior surface temperatures of the individual surfaces of each zone.
+        surfaceAnalysis_: Set to "True" to have EnergyPlus solve for the gains and losses through the individual surfaces of each zone.
+        surfaceAnalysis_: Set to "True" to have EnergyPlus solve for the transmitted beam, diffuse, and total solar gain through the individual window surfaces of each zone.
         ____________________: ...
         timestep_: Specify a timestep by inputing the words 'hourly', 'daily', 'monthly' or 'annually'.  The default is set to hourly.
     Returns:
@@ -25,7 +27,7 @@ Provided by Honeybee 0.0.53
 
 ghenv.Component.Name = "Honeybee_Write EP Result Parameters"
 ghenv.Component.NickName = 'writeResultParameters'
-ghenv.Component.Message = 'VER 0.0.53\nAUG_07_2014'
+ghenv.Component.Message = 'VER 0.0.53\nAUG_09_2014'
 ghenv.Component.Category = "Honeybee"
 ghenv.Component.SubCategory = "09 | Energy | Energy"
 ghenv.Component.AdditionalHelpFromDocStrings = "3"
@@ -33,7 +35,7 @@ ghenv.Component.AdditionalHelpFromDocStrings = "3"
 
 
 
-def main(zoneEnergyUse, zoneGainsAndLosses, zoneComfortMetrics, surfaceAnalysis, zoneHVACMetrics, timestep):
+def main(zoneEnergyUse, zoneGainsAndLosses, zoneComfortMetrics, zoneHVACMetrics, surfaceTempAnalysis, surfaceEnergyAnalysis, glazingSolarAnalysis, timestep):
     simulationOutputs = []
     timePeriod = timestep + ";"
     
@@ -44,8 +46,6 @@ def main(zoneEnergyUse, zoneGainsAndLosses, zoneComfortMetrics, surfaceAnalysis,
         simulationOutputs.append("Output:Variable,*,Zone Ideal Loads Zone Total Heating Energy, " + timePeriod)
         simulationOutputs.append("Output:Variable,*,Zone Lights Electric Energy, " + timePeriod)
         simulationOutputs.append("Output:Variable,*,Zone Electric Equipment Electric Energy, " + timePeriod)
-    else:
-        pass
     
     if zoneGainsAndLosses == True:
         simulationOutputs.append("Output:Variable,*,Zone Windows Total Transmitted Solar Radiation Energy, " + timePeriod)
@@ -54,16 +54,12 @@ def main(zoneEnergyUse, zoneGainsAndLosses, zoneComfortMetrics, surfaceAnalysis,
         simulationOutputs.append("Output:Variable,*,Zone People Total Heating Energy, " + timePeriod)
         simulationOutputs.append("Output:Variable,*,Zone Infiltration Total Heat Loss Energy, " + timePeriod)
         simulationOutputs.append("Output:Variable,*,Zone Infiltration Total Heat Gain Energy, " + timePeriod)
-    else:
-        pass
     
     if zoneComfortMetrics == True:
         simulationOutputs.append("Output:Variable,*,Zone Operative Temperature, " + timePeriod)
         simulationOutputs.append("Output:Variable,*,Zone Mean Air Temperature, " + timePeriod)
         simulationOutputs.append("Output:Variable,*,Zone Mean Radiant Temperature, " + timePeriod)
         simulationOutputs.append("Output:Variable,*,Zone Air Relative Humidity, " + timePeriod)
-    else:
-        pass
     
     if zoneHVACMetrics == True:
         simulationOutputs.append("Output:Variable,*,Zone Ideal Loads Zone Latent Heating Energy, " + timePeriod)
@@ -73,20 +69,20 @@ def main(zoneEnergyUse, zoneGainsAndLosses, zoneComfortMetrics, surfaceAnalysis,
         simulationOutputs.append("Output:Variable,*,System Node Mass Flow Rate, " + timePeriod)
         simulationOutputs.append("Output:Variable,*,System Node Temperature, " + timePeriod)
         simulationOutputs.append("Output:Variable,*,System Node Relative Humidity, " + timePeriod)
-    else:
-        pass
     
-    if surfaceAnalysis == True:
+    if surfaceTempAnalysis == True:
         simulationOutputs.append("Output:Variable,*,Surface Outside Face Temperature, " + timePeriod)
         simulationOutputs.append("Output:Variable,*,Surface Inside Face Temperature, " + timePeriod)
+    
+    if surfaceEnergyAnalysis == True:
         simulationOutputs.append("Output:Variable,*,Surface Average Face Conduction Heat Transfer Energy, " + timePeriod)
         simulationOutputs.append("Output:Variable,*,Surface Window Heat Loss Energy, " + timePeriod)
         simulationOutputs.append("Output:Variable,*,Surface Window Heat Gain Energy, " + timePeriod)
+    
+    if glazingSolarAnalysis_ == True:
         simulationOutputs.append("Output:Variable,*,Surface Window Transmitted Beam Solar Radiation Energy, " + timePeriod)
         simulationOutputs.append("Output:Variable,*,Surface Window Transmitted Diffuse Solar Radiation Energy, " + timePeriod)
         simulationOutputs.append("Output:Variable,*,Surface Window Transmitted Solar Radiation Energy, " + timePeriod)
-    else:
-        pass
     
     return simulationOutputs
 
@@ -98,5 +94,5 @@ else:timestep_ = "hourly"
 
 
 #Generate the simulation outputs if the above checks are sucessful.
-simulationOutputs = main(zoneEnergyUse_, zoneGainsAndLosses_, zoneComfortMetrics_, surfaceAnalysis_, zoneHVACParams_, timestep_)
+simulationOutputs = main(zoneEnergyUse_, zoneGainsAndLosses_, zoneComfortMetrics_, zoneHVACParams_, surfaceTempAnalysis_, surfaceEnergyAnalysis_, glazingSolarAnalysis_, timestep_)
 print "Simulation outputs generated successfully!"
