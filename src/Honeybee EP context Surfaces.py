9a9b81c31833835d8a5809821751fd8893b8264d
Chris Mackey <chris@mackeyarchitecture.com>
2014-08-15 13:27:38 -0400

    Shd Context Now Writes Rad Mat to Lib

--- src/Honeybee EP context Surfaces.py
+++ src/Honeybee EP context Surfaces.py
@@ -12,7 +12,7 @@ prepare shading/context geometries
 
 ghenv.Component.Name = 'Honeybee EP context Surfaces'
 ghenv.Component.NickName = 'HB_EPContextSrf'
-ghenv.Component.Message = 'VER 0.0.53\nAUG_07_2014'
+ghenv.Component.Message = 'VER 0.0.53\nAUG_15_2014'
 ghenv.Component.Category = "Honeybee"
 ghenv.Component.SubCategory = "09 | Energy | Energy"
 ghenv.Component.AdditionalHelpFromDocStrings = "1"
@@ -29,13 +29,15 @@ tolerance = sc.doc.ModelAbsoluteTolerance
 import math
 
 
-def main():
+def main(EPConstruction, RADMaterial):
     # import the classes
     if sc.sticky.has_key('ladybug_release')and sc.sticky.has_key('honeybee_release'):
         
         # don't customize this part
         hb_EPSrf = sc.sticky["honeybee_EPSurface"]
         hb_EPSHDSurface = sc.sticky["honeybee_EPShdSurface"]
+        hb_RADMaterialAUX = sc.sticky["honeybee_RADMaterialAUX"]()
+        hb_EPObjectsAux = sc.sticky["honeybee_EPObjectsAUX"]()
 
         ########################################################################
         #----------------------------------------------------------------------#
@@ -76,8 +78,66 @@ def main():
                         
                         thisShading = hb_EPSHDSurface(shdBrep, 1000*brepCount + meshFace, 'shdSrf_' + `brepCount` + '_' + `meshFace` + "_" + str(uuid.uuid4()))
                         
-                        if EPConstruction!=None: thisShading.EPConstruction = EPConstruction
-                        if RADConstruction!=None: thisShading.RadMaterial = getRadMaterialName(RADConstruction)
+                        #if EPConstruction!=None: thisShading.EPConstruction = EPConstruction
+                        #if RADConstruction!=None: thisShading.RadMaterial = getRadMaterialName(RADConstruction)
+                        if EPConstruction!=None:
+                            # if it is just the name of the material make sure it is already defined
+                            if len(EPConstruction.split("\n")) == 1:
+                                # if the material is not in the library add it to the library
+                                if not hb_EPObjectsAux.isEPConstruction(EPConstruction):
+                                    warningMsg = "Can't find " + EPConstruction + " in EP Construction Library.\n" + \
+                                                "Add the construction to the library and try again."
+                                    ghenv.Component.AddRuntimeMessage(gh.GH_RuntimeMessageLevel.Warning, warningMsg)
+                                    return
+                            else:
+                                # it is a full string
+                                added, EPConstruction = hb_EPObjectsAux.addEPObjectToLib(EPConstruction, overwrite = True)
+                
+                                if not added:
+                                    msg = name + " is not added to the project library!"
+                                    ghenv.Component.AddRuntimeMessage(gh.GH_RuntimeMessageLevel.Warning, msg)
+                                    print msg
+                                    return
+                            
+                            try:
+                                thisShading.setEPConstruction(EPConstruction)
+                            except:
+                                warningMsg = "You are using an old version of Honeybee_Honeybee! Update your files and try again."
+                                print warningMsg
+                                ghenv.Component.AddRuntimeMessage(gh.GH_RuntimeMessageLevel.Warning, warningMsg)
+                                return   
+                        
+                        if RADMaterial!=None:
+                            # if it is just the name of the material make sure it is already defined
+                            if len(RADMaterial.split(" ")) == 1:
+                                # if the material is not in the library add it to the library
+                                if RADMaterial not in sc.sticky ["honeybee_RADMaterialLib"].keys():
+                                    warningMsg = "Can't find " + RADMaterial + " in RAD Material Library.\n" + \
+                                                "Add the material to the library and try again."
+                                    ghenv.Component.AddRuntimeMessage(gh.GH_RuntimeMessageLevel.Warning, warningMsg)
+                                    return
+                                
+                                try:
+                                    thisShading.setRADMaterial(RADMaterial)
+                                except Exception, e:
+                                    print e
+                                    warningMsg = "You are using an old version of Honeybee_Honeybee! Update your files and try again."
+                                    print warningMsg
+                                    ghenv.Component.AddRuntimeMessage(gh.GH_RuntimeMessageLevel.Warning, warningMsg)
+                                    return
+                                
+                                addedToLib = True
+                            else:
+                                
+                                # try to add the material to the library
+                                addedToLib, thisShading.RadMaterial = hb_RADMaterialAUX.analyseRadMaterials(RADMaterial, True)
+                                
+                            if addedToLib==False:
+                                warningMsg = "Failed to add " + RADMaterial + " to the Library."
+                                ghenv.Component.AddRuntimeMessage(gh.GH_RuntimeMessageLevel.Warning, warningMsg)
+                                return
+                        
+                        
                         shadingClasses.append(thisShading)
             
         # add to the hive
@@ -94,6 +154,6 @@ def main():
 
 if shdSurfaces and shdSurfaces[0]!=None:
     # add cleaning function
-    result= main()
+    result= main(EPConstruction, RADMaterial)
     
     if result != -1: HBContext = result
