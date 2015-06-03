e066379523d713c83927a37bc1b89542bd44b464
Chris Mackey <chris@mackeyarchitecture.com>
2015-06-03 19:24:30 -0400

    Fixed a Small Bug

--- src/Honeybee_SplitBuildingMass.py
+++ src/Honeybee_SplitBuildingMass.py
@@ -16,7 +16,7 @@ Provided by Honeybee 0.0.56
 
     Args:
         _bldgMasses: A Closed brep or list of closed breps representing a building massing.
-        bldgsFlr2FlrHeights_: A list of floor heights in Rhino model units that will be used to make each floor of the building.  The list should run from bottom floor to top floor.  Alternatively, you can input a text string that codes for how many floors of each height you want.  For example, inputting "2@4" (without quotations) will make two ground floors with a height of 4 Rhino model units.  Simply typing "@3" will make all floors 3 Rhino model units.  Putting in lists of these text strings will divide up floors accordingly.  For example, the list "1@5   2@4   @3"  will make a ground floor of 5 units, two floors above that at 4 units and all remaining floors at 3 units.
+        bldgsFlr2FloorHeights_: A list of floor heights in Rhino model units that will be used to make each floor of the building.  The list should run from bottom floor to top floor.  Alternatively, you can input a text string that codes for how many floors of each height you want.  For example, inputting "2@4" (without quotations) will make two ground floors with a height of 4 Rhino model units.  Simply typing "@3" will make all floors 3 Rhino model units.  Putting in lists of these text strings will divide up floors accordingly.  For example, the list "1@5   2@4   @3"  will make a ground floor of 5 units, two floors above that at 4 units and all remaining floors at 3 units.
         perimeterZoneDepth_: A list of perimeter zone depths in Rhino model units that will be used to divide up each floor of the building into core and perimeter zones.  The list should run from bottom floor to top floor.  Alternatively, you can input a text string that codes for which floors you want at which zone depth.  For example, inputting "2@4" (without quotations) will divide up the two ground floors with a perimeter zone depth of 4 Rhino model units.  Simply typing "@3" will divide up all floors with a zone depth of 3 Rhino model units.  Putting in lists of these text strings will divide up floors accordingly.  For example, the list "1@5   2@4   @3"  will make a ground floor divided up with a zone depth of 5 units, two floors divided at 4 units and all remaining floors at 3 units.
         _createHoneybeeZones: Set Boolean to "True" to split up the building mass into geometries for zones.
     Returns:
@@ -27,7 +27,7 @@ Provided by Honeybee 0.0.56
 
 ghenv.Component.Name = 'Honeybee_SplitBuildingMass'
 ghenv.Component.NickName = 'SplitMass'
-ghenv.Component.Message = 'VER 0.0.56\nFEB_01_2015'
+ghenv.Component.Message = 'VER 0.0.56\nJUN_03_2015'
 ghenv.Component.Category = "Honeybee"
 ghenv.Component.SubCategory = "00 | Honeybee"
 #compatibleHBVersion = VER 0.0.56\nFEB_01_2015
@@ -142,7 +142,7 @@ def getFloorCrvs(buildingMass, floorHeights, maxHeights):
         elif len(crvList) == 1 and count == 0:
             cntrCrvs.append([crvList[0]])
         elif len(crvList) == 1 and count != 0:
-            cntrCrvs[0].append(crvList[0])
+            cntrCrvs.append(crvList)
         else: pass
         
         if crvList != []:
