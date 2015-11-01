3902ae98fedcc69f556556af5a02eced79a53e29
Chris Mackey <chris@mackeyarchitecture.com>
2015-10-31 21:54:22 -0400

    Forgot to Update Compatible HB Version

--- src/Honeybee_Export To OpenStudio.py
+++ src/Honeybee_Export To OpenStudio.py
@@ -64,7 +64,7 @@ ghenv.Component.NickName = 'exportToOpenStudio'
 ghenv.Component.Message = 'VER 0.0.57\nOCT_31_2015'
 ghenv.Component.Category = "Honeybee"
 ghenv.Component.SubCategory = "09 | Energy | Energy"
-#compatibleHBVersion = VER 0.0.56\nOCT_26_2015
+#compatibleHBVersion = VER 0.0.56\nOCT_31_2015
 #compatibleLBVersion = VER 0.0.59\nFEB_01_2015
 ghenv.Component.AdditionalHelpFromDocStrings = "1"
 
