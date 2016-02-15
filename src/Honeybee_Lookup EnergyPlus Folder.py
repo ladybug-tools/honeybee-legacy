161f1f12906e87dc81877d600dc441b2851bf958
ayezioro <ayezioro@gmail.com>
2016-02-15 09:00:28 +0200

    Revert "New commit."
    This reverts commit 19b38deaa4e046be54937b244312a038813decaa.

--- src/Honeybee_Lookup EnergyPlus Folder.py
+++ src/Honeybee_Lookup EnergyPlus Folder.py
@@ -44,7 +44,7 @@ Provided by Honeybee 0.0.59
 """
 ghenv.Component.Name = "Honeybee_Lookup EnergyPlus Folder"
 ghenv.Component.NickName = 'LookupFolder_EnergyPlus'
-ghenv.Component.Message = 'VER 0.0.59\nFEB_15_2016'
+ghenv.Component.Message = 'VER 0.0.59\nFEB_14_2016'
 ghenv.Component.IconDisplayMode = ghenv.Component.IconDisplayMode.application
 ghenv.Component.Category = "Honeybee"
 ghenv.Component.SubCategory = "09 | Energy | Energy"
