62084244d502aca4298a1c98f030a1ba9f1052b5
Chris Mackey <chris@mackeyarchitecture.com>
2016-03-24 13:18:42 -0400

    Turned Component Preview On

--- src/Honeybee_Create Therm Polygons.py
+++ src/Honeybee_Create Therm Polygons.py
@@ -47,7 +47,7 @@ import decimal
 
 ghenv.Component.Name = 'Honeybee_Create Therm Polygons'
 ghenv.Component.NickName = 'createThermPolygons'
-ghenv.Component.Message = 'VER 0.0.59\nMAR_20_2016'
+ghenv.Component.Message = 'VER 0.0.59\nMAR_24_2016'
 ghenv.Component.IconDisplayMode = ghenv.Component.IconDisplayMode.application
 ghenv.Component.Category = "Honeybee"
 ghenv.Component.SubCategory = "11 | THERM"
