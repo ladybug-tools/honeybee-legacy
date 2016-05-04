cefec997f4f974394d9cec7655eb85cad7770d14
Chris Mackey <chris@mackeyarchitecture.com>
2016-05-04 17:44:03 -0400

    Forgot to commit .py

--- src/Honeybee_OpenStudio Heating Details.py
+++ src/Honeybee_OpenStudio Heating Details.py
@@ -41,7 +41,7 @@ import Grasshopper.Kernel as gh
 
 ghenv.Component.Name = "Honeybee_OpenStudio Heating Details"
 ghenv.Component.NickName = 'HeatingDetails'
-ghenv.Component.Message = 'VER 0.0.59\nMAY_01_2016'
+ghenv.Component.Message = 'VER 0.0.59\nMAY_04_2016'
 ghenv.Component.IconDisplayMode = ghenv.Component.IconDisplayMode.application
 ghenv.Component.Category = "Honeybee"
 ghenv.Component.SubCategory = "10 | Energy | HVACSystems"
@@ -54,6 +54,7 @@ except: pass
 import scriptcontext as sc
 import Grasshopper.Kernel as gh
 
+w = gh.GH_RuntimeMessageLevel.Warning
 
 def main(hb_heatingDetail):
     myHeatDetails = hb_heatingDetail(_heatingAvailSched_, _heatingEffOrCOP_, supplyTemperature_, \
