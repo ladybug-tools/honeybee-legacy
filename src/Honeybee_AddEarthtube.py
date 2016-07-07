55889b06fd5149c5f4358252484fbb0c1a6418d3
Chris Mackey <chris@mackeyarchitecture.com>
2016-07-07 19:16:11 -0400

    Moving to WIP Until It Supports OpenStudio

--- src/Honeybee_AddEarthtube.py
+++ src/Honeybee_AddEarthtube.py
@@ -114,13 +114,13 @@ Provided by Honeybee 0.0.59
 """
 
 ghenv.Component.Name = "Honeybee_AddEarthtube"
-ghenv.Component.Message = 'VER 0.0.59\nJAN_26_2016'
+ghenv.Component.Message = 'VER 0.0.59\nJUL_07_2016'
 ghenv.Component.IconDisplayMode = ghenv.Component.IconDisplayMode.application
 ghenv.Component.Category = "Honeybee"
-ghenv.Component.SubCategory = "08 | Energy | Set Zone Properties"
+ghenv.Component.SubCategory = "13 | WIP" #"08 | Energy | Set Zone Properties"
 #compatibleHBVersion = VER 0.0.56\nFEB_01_2015
 #compatibleLBVersion = VER 0.0.59\nFEB_01_2015
-try: ghenv.Component.AdditionalHelpFromDocStrings = "0"
+try: ghenv.Component.AdditionalHelpFromDocStrings = "3"
 except: pass
 
 import scriptcontext as sc
