885c22e137c775fda237ae95d56fd9fe2f25c3c9
Chris Mackey <chris@mackeyarchitecture.com>
2018-05-05 13:41:19 -0400

    Grizzly Bear Now Imports Boundary Conditions

--- src/Honeybee_gbXML to Honeybee.py
+++ src/Honeybee_gbXML to Honeybee.py
@@ -47,7 +47,7 @@ Provided by Honeybee 0.0.63
 
 ghenv.Component.Name = "Honeybee_gbXML to Honeybee"
 ghenv.Component.NickName = 'XMLTOHB'
-ghenv.Component.Message = 'VER 0.0.63\nJAN_20_2018'
+ghenv.Component.Message = 'VER 0.0.63\nMAY_05_2018'
 ghenv.Component.IconDisplayMode = ghenv.Component.IconDisplayMode.application
 ghenv.Component.Category = "Honeybee"
 ghenv.Component.SubCategory = "10 | Energy | Energy"
@@ -58,7 +58,6 @@ ghenv.Component.AdditionalHelpFromDocStrings = "0"
 import os
 import scriptcontext as sc
 import Rhino as rc
-
 
 def getHBMaterial(m):
     mat = {}
@@ -125,19 +124,86 @@ def getHBSrfType(ossurface):
     if t in srfTypeDict:
         return srfTypeDict[t]
 
-def getOSSurfaceConstructionName(s, constructionCollection):
+def getOSSurfaceConstructionName(s, constructionCollection, missingConstrCount = 0):
     construction = s.construction()
     if construction.is_initialized and not construction.isNull():
         construction = construction.get()
     if not hasattr(construction, 'name'):
-        print 'Construction for {} is missing. Default construction will be used.'.format(s.name())
-        return None
+        missingConstrCount += 1
+        #print 'Construction for {} is missing. Default construction will be used.'.format(s.name())
+        return None, missingConstrCount
     else:
         if not str(construction.name()) in constructionCollection:
             print 'Failed to find {} in constructions.'.format(construction.name())
-            return None
+            return None, missingConstrCount
         else:
-            return str(construction.name())
+            return str(construction.name()), missingConstrCount
+
+def updateAdj(surface1, surface2):
+    # change roof to ceiling
+    # the same for ceiling on ground
+    if int(surface1.type) == 1: surface1.setType(3) # roof + adjacent surface = ceiling
+    elif int(surface2.type) == 1: surface2.setType(3)
+    
+    # Change different floor types to be floors between zones.
+    if int(surface1.type) == 2: surface1.setType(2)
+    if int(surface2.type) == 2:  surface2.setType(2)
+    
+    # change construction
+    if surface1.EPConstruction == None:
+        surface1.setEPConstruction(surface1.intCnstrSet[surface1.type])
+        surface2.setEPConstruction(surface1.intCnstrSet[surface2.type])
+    else:
+        hb_EPObjectsAux = sc.sticky["honeybee_EPObjectsAUX"]()
+        hb_EPObjectsAux.assignEPConstruction(surface1, surface1.EPConstruction, ghenv.Component)
+        hb_EPObjectsAux.assignEPConstruction(surface2, surface1.EPConstruction, ghenv.Component)
+    
+    # change bc
+    surface1.setBC('SURFACE', True)
+    surface2.setBC('SURFACE', True)
+    # change bc.Obj
+    # used to be only a name but I changed it to an object so you can find the parent, etc.
+    surface1.setBCObject(surface2)
+    surface2.setBCObject(surface1)
+    
+    # set sun and wind exposure to no exposure
+    surface2.setSunExposure('NoSun')
+    surface1.setSunExposure('NoSun')
+    surface2.setWindExposure('NoWind')
+    surface1.setWindExposure('NoWind')
+    
+    # check for child objects
+    if (surface1.hasChild or surface2.hasChild) and (len(surface2.childSrfs)!= len(surface1.childSrfs)):
+        # give warning
+        warnMsg= "Number of windows doesn't match between " + surface1.name + " and " + surface2.name + "." + \
+                 " Make sure adjacent surfaces are divided correctly and have similar windows."
+        print warnMsg
+    
+    if surface1.hasChild and surface2.hasChild:
+        # find child surfaces that match the other one
+        for childSurface1 in surface1.childSrfs:
+            for childSurface2 in surface2.childSrfs:
+                if childSurface1.cenPt.DistanceTo(childSurface2.cenPt) <= sc.doc.ModelAbsoluteTolerance:
+                    childSurface1.BCObject.name = childSurface2.name
+                    childSurface2.BCObject.name = childSurface1.name
+                    # change construction
+                    if childSurface1.EPConstruction == None:
+                        childSurface1.setEPConstruction(surface1.intCnstrSet[5])
+                        childSurface2.setEPConstruction(surface1.intCnstrSet[5])
+                    else:
+                        hb_EPObjectsAux = sc.sticky["honeybee_EPObjectsAUX"]()
+                        hb_EPObjectsAux.assignEPConstruction(childSurface1, childSurface1.EPConstruction, ghenv.Component)
+                        hb_EPObjectsAux.assignEPConstruction(childSurface2, childSurface1.EPConstruction, ghenv.Component)
+                    # change the boundary condition
+                    childSurface1.setBC('SURFACE', True)
+                    childSurface2.setBC('SURFACE', True)
+                    childSurface1.setBCObject(childSurface2)
+                    childSurface2.setBCObject(childSurface1)
+                    # set sun and wind exposure to no exposure
+                    childSurface2.setSunExposure('NoSun')
+                    childSurface1.setSunExposure('NoSun')
+                    childSurface2.setWindExposure('NoWind')
+                    childSurface1.setWindExposure('NoWind')
 
 if sc.sticky.has_key('honeybee_release'):
 
@@ -207,8 +273,13 @@ if openStudioIsReady and _import and _filepath:
                 .format(m.name(), c.name())
             materialCollection[str(m.name())] = getHBMaterial(m)
     
-    
+    # list of final HBzones
     zones = []
+    # dictionary to store boundary consition and adjacency information.
+    adjDict = {}
+    # number of surfaces with missing construcitons
+    missingCcount = 0
+    adjResetNeeded = False
     
     if not _zoneNames:
         spaces = model.getSpaces()
@@ -228,22 +299,61 @@ if openStudioIsReady and _import and _filepath:
             minZ = min(p.z() for p in s.vertices())
             maxZ = max(p.z() for p in s.vertices())
             srf = EPZoneSurface(getGeometry(s), 1, str(s.name()), getHBSrfType(s))
-            construction = getOSSurfaceConstructionName(s, constructionCollection)
+            construction, missingCcount = getOSSurfaceConstructionName(s, constructionCollection, missingCcount)
             if construction:
                 srf.construction = construction
+            
+            # store adjacency information
+            HsrfName = srf.name
+            if str(s.outsideBoundaryCondition()) == 'Ground':
+                adjDict[HsrfName] = ['GROUND', None]
+                adjResetNeeded = True
+            elif str(s.outsideBoundaryCondition()) == 'Adiabatic':
+                adjDict[HsrfName] = ['ADIABATIC', None]
+                adjResetNeeded = True
+            elif str(s.outsideBoundaryCondition()) == 'Surface':
+                adjDict[HsrfName] = ['SURFACE', str(s.adjacentSurface().get().name())]
+                adjResetNeeded = True
+            else:
+                adjDict[HsrfName] = ['OUTDOORS', None]
+            
             hbz.addSrf(srf)
             for ss in s.subSurfaces():
                 #create the surface
                 fenSrf = EPFenSurface(getGeometry(ss, minZ, maxZ), 1, str(ss.name()), srf, 5)
-                construction = getOSSurfaceConstructionName(ss, constructionCollection)
+                construction, missingCcount = getOSSurfaceConstructionName(ss, constructionCollection, missingCcount)
                 if construction:
                    fenSrf.construction = construction
                 srf.addChildSrf(fenSrf)
         
         zones.append(hbz)
     
+    # create the zones
     for zone in zones:
         zone.createZoneFromSurfaces()
+    
+    # try to edit the zone adjacencies to reflect what is in the gbXML.
+    if adjResetNeeded == True:
+        for zone in zones:
+            for srf in zone.surfaces:
+                if adjDict[srf.name][0] == 'SURFACE':
+                    adjSrfName = adjDict[srf.name][1]
+                    aSrfFound = False
+                    if adjSrfName != None:
+                        # try to find the adjacent surface among the zones.
+                        for azone in zones:
+                            for asrf in azone.surfaces:
+                                if asrf.name == adjSrfName:
+                                    aSrfFound = True
+                                    updateAdj(srf, asrf)
+                elif adjDict[srf.name][0] != 'OUTDOORS':
+                    srf.BC = adjDict[srf.name][0]
+                    srf.setSunExposure('NoSun')
+                    srf.setSunExposure('NoSun')
+    
+    # give warnings about missing constructions.
+    if missingCcount != 0:
+        print '{} surfaces have missing constructions. Default construction will be used.'.format(str(missingCcount))
     
     shadings = (EPSHDSurface(getGeometry(shd), 1, str(shd.name()))
                 for shd in model.getShadingSurfaces())
@@ -254,6 +364,9 @@ if openStudioIsReady and _import and _filepath:
     
     hb_hive = sc.sticky["honeybee_Hive"]()
     HBZones = hb_hive.addToHoneybeeHive(zones, ghenv.Component)
+    
+    
+    
     try:
         shadings = hb_hive.addToHoneybeeHive(shadings, ghenv.Component, False)
     except TypeError:
