d7d18a80f32bfce6ce91ce0e82204b173fcf6c57
Chris Mackey <chris@mackeyarchitecture.com>
2018-10-30 21:59:35 -0400

    Updated to Work with Newer Versions of E+

--- src/Honeybee_Set EnergyPlus Zone Thresholds.py
+++ src/Honeybee_Set EnergyPlus Zone Thresholds.py
@@ -35,9 +35,8 @@ Provided by Honeybee 0.0.63
         maxHumidity_: A number or list of numbers that represent the maximum relative humidity allowed by a humidistat in %.  The HVAC will dehumidify the zone air  if the relative humidity goes above this threshold.  The default is set to 'no limit' or no humidistat. This can be either a single number to be applied to all connected zones or a list of numbers for each different zone.
         minHumidity_ : A number or list of numbers that represent the minimum relative humidity allowed by a humidistat in %.  The HVAC will humidify the zone air if the relative humidity goes below this threshold.  The default is set to 'no limit' or no humidistat. This can be either a single number to be applied to all connected zones or a list of numbers for each different zone.
         outdoorAirReq_: An integer or text string value that changes the outdoor air requirement of the zone (the default is set to "0 - Sum").  Choose from the following options:
-            0 - Sum - The outdoor air coming through the mechnical system will be the sum of the specified flow/m2 of zone floor area and the flow/person.  This is the default and is the usual recommendation of ASHRAE.
-            1 - Maximum - The outdoor air coming through the mechnical system will be either the specified flow/m2 of zone floor area or the flow/person (depending on which is larger at a given hour).   Choosing this option effectively implies that there is a demand-controlled ventilation system set up in the zone.
-            2 - None - No outdoor air will come through the mechanical system and the heating/cooling will be applied only through re-circulation of indoor air.  Be careful as this option might not bring enough fresh air to occupants if the zone's infiltration is very low.
+            0 - Sum - means that the flows calculated from the fields Outdoor Air Flow per Person, Outdoor Air Flow per Area, Outdoor Air Flow per Zone, and Air Changes per Hour (using the associated conversions to m3/s for each field) will be added to obtain the zone outdoor air flow rate.
+            1 - Maximum - means that the maximum flow derived from Outdoor Air Flow per Person, Outdoor Air Flow per Area, Outdoor Air Flow per Zone, and Air Changes per Hour (using the associated conversions to m3/s for each field) will be used as the zone outdoor air flow rate.
         daylightIllumSetPt_: A number of list of numbers that represent the illuminance threshold in lux beyond which electric lights will be dimmed if there is sufficent daylight.  The default has no dimming for daylight, meaning that lights will be on whenever the schedule states that they are on (regardless of daylight).  If you specify a daylightCntrlFract_ below, this component will automatically assume a setpoint of 300 lux.  Some other common setpoints are:
             50 lux - Corridors and hallways
             150 lux - Spaces where people are working on computer screens, which already provide their own light.
@@ -52,7 +51,7 @@ Provided by Honeybee 0.0.63
 
 ghenv.Component.Name = "Honeybee_Set EnergyPlus Zone Thresholds"
 ghenv.Component.NickName = 'setEPZoneThresholds'
-ghenv.Component.Message = 'VER 0.0.63\nJAN_20_2018'
+ghenv.Component.Message = 'VER 0.0.63\nOCT_30_2018'
 ghenv.Component.IconDisplayMode = ghenv.Component.IconDisplayMode.application
 ghenv.Component.Category = "Honeybee"
 ghenv.Component.SubCategory = "08 | Energy | Set Zone Properties"
@@ -108,16 +107,9 @@ def checkTheInputs():
     outdoorAirReqFinal = []
     checkData = True
     for outAirReq in outdoorAirReq:
-        if outAirReq == "0" or outAirReq == "1" or outAirReq == "2" or outAirReq.upper() == "NONE" or outAirReq.upper() == "MAXIMUM" or outAirReq == "SUM":
-            if outAirReq == "0": outdoorAirReqFinal.append("Sum")
-            elif outAirReq == "1": outdoorAirReqFinal.append("Maximum")
-            elif outAirReq == "2": outdoorAirReqFinal.append("None")
-            else: outdoorAirReqFinal.append(outAirReq)
-        else:
-            checkData = False
-            msg = "Invalid outdoorAirReq_ input."
-            print msg
-            ghenv.Component.AddRuntimeMessage(gh.GH_RuntimeMessageLevel.Warning, msg)
+        if outAirReq == "0": outdoorAirReqFinal.append("Sum")
+        elif outAirReq == "1": outdoorAirReqFinal.append("Maximum")
+        else: outdoorAirReqFinal.append(outAirReq)
     outdoorAirReq = outdoorAirReqFinal
     
     return coolingSetPt, coolingSetback, heatingSetPt, heatingSetback, maxHumidity, minHumidity, outdoorAirReq, daylightIllumSetPt, daylightCntrlPt, daylightCntrlFract
