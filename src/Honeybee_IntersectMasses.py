f7a9e1633e3827821a461cbfe494c332e8a74c88
Chris Mackey <chris@mackeyarchitecture.com>
2019-01-30 08:06:42 -0500

    Updating Component Date

--- src/Honeybee_IntersectMasses.py
+++ src/Honeybee_IntersectMasses.py
@@ -38,7 +38,7 @@ Provided by Honeybee 0.0.64
 """
 ghenv.Component.Name = "Honeybee_IntersectMasses"
 ghenv.Component.NickName = 'IntersectMass'
-ghenv.Component.Message = 'VER 0.0.64\nNOV_20_2018'
+ghenv.Component.Message = 'VER 0.0.64\nJAN_30_2019'
 ghenv.Component.IconDisplayMode = ghenv.Component.IconDisplayMode.application
 ghenv.Component.Category = "Honeybee"
 ghenv.Component.SubCategory = "00 | Honeybee"
